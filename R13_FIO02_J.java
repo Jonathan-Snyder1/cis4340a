38e6409cb8abe301bea7a11107f8774d8313cd36
Jonathan Snyder <125504064+Jonathan-Snyder1@users.noreply.github.com>
2026-03-08 18:19:40 -0400

    Update R13_FIO02_J.java compliant

--- R13_FIO02_J.java
+++ R13_FIO02_J.java
@@ -1,5 +1,6 @@
 //FIO02 Detect and handle file-related errors
-//non compliant version return value is ignored
+//compliant version 
+//fix by checking file deletion result
 
 //import
 import java.io.File;
@@ -7,7 +8,11 @@ import java.io.File;
 public class R13_FIO02_J {
     public static void main(String[] args) {
         File file = new File("temp.txt");
-        file.delete(); // ignores whether deletion succeeded
-        System.out.println("Finished.");
+
+        if (!file.delete()) {
+            System.out.println("Failed to delete temp.txt");
+        } else {
+            System.out.println("Deleted temp.txt successfully");
+        }
     }
 }
