5cc8e2237b6cf13453c79ec582286ee059b2b327
Jonathan Snyder <125504064+Jonathan-Snyder1@users.noreply.github.com>
2026-03-08 17:30:36 -0400

    Update R00_IDS03_J.java compliant

--- R00_IDS03_J.java
+++ R00_IDS03_J.java
@@ -1,12 +1,21 @@
+import java.util.regex.Pattern;
+
+#compliant code
 public class R00_IDS03_J {
     public static void main(String[] args) {
         String username = "bad$user";
         boolean loginSuccessful = false;
 
         if (loginSuccessful) {
-            System.err.println("User login succeeded for: " + username);
+            System.err.println("User login succeeded for: " + sanitizeUser(username));
         } else {
-            System.err.println("User login failed for: " + username);
+            System.err.println("User login failed for: " + sanitizeUser(username));
         }
+    }
+
+    public static String sanitizeUser(String username) {
+        return Pattern.matches("[A-Za-z0-9 ]+", username)
+                ? username
+                : "unauthorized user";
     }
 }
