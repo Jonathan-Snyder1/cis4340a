7d6f27b450b9ce90ce1872ac539e54d1f9d0de7c
Jonathan Snyder <125504064+Jonathan-Snyder1@users.noreply.github.com>
2026-03-08 17:50:26 -0400

    Update R04_STR03_J.java compliant

--- R04_STR03_J.java
+++ R04_STR03_J.java
@@ -1,15 +1,15 @@
 import java.math.BigInteger;
 
-//noncompliant version
-// convert BigInteger to byte array, make string from bytes then convert back
+//compliant version
+//fix by converting numeric data with toString
 
 public class R04_STR03_J {
     public static void main(String[] args) {
         BigInteger x = new BigInteger("530500452766");
-        byte[] byteArray = x.toByteArray();
-        String s = new String(byteArray);
-        byteArray = s.getBytes();
-        x = new BigInteger(byteArray);
+        String s = x.toString(); // Valid character data
+        byte[] byteArray = s.getBytes();
+        String ns = new String(byteArray);
+        x = new BigInteger(ns);
 
         System.out.println(x);
     }
