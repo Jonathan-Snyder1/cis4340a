de5c9000738039108469dd6d3020e2ab319aba34
Jonathan Snyder <125504064+Jonathan-Snyder1@users.noreply.github.com>
2026-03-08 18:25:45 -0400

    Update R15_SEC05_J.java compliant

--- R15_SEC05_J.java
+++ R15_SEC05_J.java
@@ -1,24 +1,20 @@
 //SEC05 Do not use reflection to increrase accessibilty of classes, methods, or fields
-//non compliant version
-// Using reflection can break encapsulation, and cause security vulnerabilities
-
-import java.lang.reflect.Field;
+//compliant version
+//fix by removing reflective access to private field
 
 
 public class R15_SEC05_J {
 
     private static class SecretData {
         private String password = "superSecret123";
+
+        public String getMaskedPassword() {
+            return "********";
+        }
     }
 
     public static void main(String[] args) {
-        try {
-            SecretData data = new SecretData();
-            Field field = SecretData.class.getDeclaredField("password");
-            field.setAccessible(true); // Noncompliant
-            System.out.println(field.get(data));
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        SecretData data = new SecretData();
+        System.out.println(data.getMaskedPassword());
     }
 }
